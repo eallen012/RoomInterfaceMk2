001399b2e599828b9dff6212be603641e2fc021d
erika <eallen121203@gmail.com>
2026-06-09 13:19:43 -0700

    gets tiles from osm, even if they are not correct

--- Modules/RadarDisplay/RadarTile.py
+++ Modules/RadarDisplay/RadarTile.py
@@ -4,7 +4,7 @@ import time
 
 from PyQt6.QtCore import Qt, QTimer, QUrl
 from PyQt6.QtGui import QPixmap, QImage
-from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 from PyQt6.QtWidgets import QLabel
 
 from loguru import logger as logging
@@ -13,7 +13,9 @@ from Utils.UtilMethods import get_host
 
 
 class RadarTile(QLabel):
-    MAX_PARSE_TIME = 0.01  # Maximum time to spend parsing responses in milliseconds per parse event
+    MAX_PARSE_TIME = (
+        0.01  # Maximum time to spend parsing responses in milliseconds per parse event
+    )
 
     def __init__(self, host, parent=None, x=0, y=0):
         super().__init__(parent)
@@ -24,9 +26,19 @@ class RadarTile(QLabel):
         self.tile_y = y
         self.screen_x = 0
         self.screen_y = 0
-        self.setPixmap(QPixmap(f"Assets/MapTiles/{self.tile_x}-{self.tile_y}.png").
-                       scaled(self.width(), self.height(), Qt.AspectRatioMode.KeepAspectRatio,
-                              Qt.TransformationMode.SmoothTransformation))
+        # self.setPixmap(
+        #     QPixmap(f"Assets/MapTiles/{self.tile_x}-{self.tile_y}.png").scaled(
+        #         self.width(),
+        #         self.height(),
+        #         Qt.AspectRatioMode.KeepAspectRatio,
+        #         Qt.TransformationMode.SmoothTransformation,
+        #     )
+        # )
+
+        self.tile_manager = QNetworkAccessManager()
+        self.tile_manager.finished.connect(self.handle_tile_response)
+
+        self.request_new_tile(self.tile_x, self.tile_y)
 
         self.timestamps = []
 
@@ -35,7 +47,7 @@ class RadarTile(QLabel):
         self.setStyleSheet("background-color: white;")
         self.radar_overlay = QLabel(self)
         self.radar_overlay.setFixedSize(256, 256)
-        self.radar_overlay.setStyleSheet('background-color: transparent;')
+        self.radar_overlay.setStyleSheet("background-color: transparent;")
         self.radar_overlay.raise_()
 
         self.network_manager = QNetworkAccessManager()
@@ -75,13 +87,18 @@ class RadarTile(QLabel):
             self.outstanding_requests += 1
             self.total_frames += 1
             self.network_manager.get(
-                QNetworkRequest(QUrl(f"{get_host()}/weather/radar/{timestamp}/{self.tile_x}/{self.tile_y}/4")))
+                QNetworkRequest(
+                    QUrl(
+                        f"{get_host()}/weather/radar/{timestamp}/{self.tile_x}/{self.tile_y}/4"
+                    )
+                )
+            )
 
     def set_radar_overlay(self, timestamp):
         self.displayed_radar_image = timestamp
         for radar_image in self.radar_images:
-            if radar_image['timestamp'] == timestamp:
-                self.radar_overlay.setPixmap(QPixmap.fromImage(radar_image['image']))
+            if radar_image["timestamp"] == timestamp:
+                self.radar_overlay.setPixmap(QPixmap.fromImage(radar_image["image"]))
                 return
         self.radar_overlay.setPixmap(QPixmap())
 
@@ -89,10 +106,14 @@ class RadarTile(QLabel):
         # Defer the parsing of the response to a background loop that only runs once every 100ms to prevent
         # holding up the refresh of the main UI
         try:
-            timestamp = int(reply.url().toString().split('/')[-4])  # Extract the timestamp from the URL
+            timestamp = int(
+                reply.url().toString().split("/")[-4]
+            )  # Extract the timestamp from the URL
             self.outstanding_requests -= 1
             if str(reply.error()) != "NetworkError.NoError":
-                logging.error(f"Failed to load map tile {self.tile_x}-{self.tile_y}@{timestamp}: {reply.error()}")
+                logging.error(
+                    f"Failed to load map tile {self.tile_x}-{self.tile_y}@{timestamp}: {reply.error()}"
+                )
                 return
             self.response_queue.put(reply)
         except Exception as e:
@@ -103,37 +124,58 @@ class RadarTile(QLabel):
     def parse_responses(self):
         parse_start = time.time()
         # starting_size = len(self.radar_images)
-        while not self.response_queue.empty() and time.time() - parse_start < self.MAX_PARSE_TIME:
+        while (
+            not self.response_queue.empty()
+            and time.time() - parse_start < self.MAX_PARSE_TIME
+        ):
             reply = self.response_queue.get()
-            timestamp = int(reply.url().toString().split('/')[-4])  # Extract the timestamp from the URL
+            timestamp = int(
+                reply.url().toString().split("/")[-4]
+            )  # Extract the timestamp from the URL
             try:
                 data = reply.readAll()
                 image = QImage.fromData(data)
                 # Resize the image to 256x256 pixels
-                image = image.scaled(self.width(), self.height(),
-                                     Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+                image = image.scaled(
+                    self.width(),
+                    self.height(),
+                    Qt.AspectRatioMode.KeepAspectRatio,
+                    Qt.TransformationMode.SmoothTransformation,
+                )
                 self.radar_images.append({"timestamp": timestamp, "image": image})
                 if timestamp == self.displayed_radar_image:
                     self.radar_overlay.setPixmap(QPixmap.fromImage(image))
             except Exception as e:
-                logging.error(f"Failed to load map tile {self.tile_x}-{self.tile_y}@{timestamp}: {e}")
+                logging.error(
+                    f"Failed to load map tile {self.tile_x}-{self.tile_y}@{timestamp}: {e}"
+                )
                 logging.exception(e)
             finally:
                 reply.deleteLater()  # Clean up the reply object
         # print(f"Parsed {len(self.radar_images) - starting_size} images in {time.time() - parse_start:.2f}s")
-        if self.outstanding_requests == 0 and self.response_queue.empty() and self.loading:
+        if (
+            self.outstanding_requests == 0
+            and self.response_queue.empty()
+            and self.loading
+        ):
             # logging.info(f"Finished parsing {len(self.radar_images)} images for {self.tile_x}-{self.tile_y}")
             self.parse_timer.stop()
 
     def change_size(self, factor):
-        self.setFixedSize(round(self.width() * factor),
-                          round(self.height() * factor))
-        self.radar_overlay.setFixedSize(round(self.radar_overlay.width() * factor),
-                                        round(self.radar_overlay.height() * factor))
+        self.setFixedSize(round(self.width() * factor), round(self.height() * factor))
+        self.radar_overlay.setFixedSize(
+            round(self.radar_overlay.width() * factor),
+            round(self.radar_overlay.height() * factor),
+        )
         # Resize the map tile image to match the new size
-        self.setPixmap(QPixmap(f"Assets/MapTiles/{self.tile_x}-{self.tile_y}.png").
-                       scaled(self.width(), self.height(), Qt.AspectRatioMode.KeepAspectRatio,
-                              Qt.TransformationMode.SmoothTransformation))
+        self.setPixmap(
+            QPixmap(f"Assets/MapTiles/{self.tile_x}-{self.tile_y}.png").scaled(
+                self.width(),
+                self.height(),
+                Qt.AspectRatioMode.KeepAspectRatio,
+                Qt.TransformationMode.SmoothTransformation,
+            )
+        )
         self.radar_images.clear()
 
     def on_screen(self):
@@ -148,8 +190,43 @@ class RadarTile(QLabel):
         if parent_x < -256 or parent_y < -256:
             return False
         # If the value is greater than the parent's parent's width or height, the tile is off the right or bottom
-        if parent_x > self.parent.parent().width() or parent_y > self.parent.parent().height():
+        if (
+            parent_x > self.parent.parent().width()
+            or parent_y > self.parent.parent().height()
+        ):
             return False
         return True
 
+    def request_new_tile(self, x, y):
+        url = f"https://tile.openstreetmap.org/10/{x}/{y}.png"
 
+        request = QNetworkRequest(QUrl(url))
+        request.setHeader(
+            QNetworkRequest.KnownHeaders.ContentTypeHeader,
+            "application/x-www-form-urlencoded",
+        )
+        request.setRawHeader(b"User-Agent", b"RoomInterfactMk2/0.0")
+
+        self.tile_manager.get(request)
+
+    def handle_tile_response(self, reply):
+        try:
+            if reply.error() != QNetworkReply.NetworkError.NoError:
+
+                print("Error")
+                print(reply.errorString())
+                return
+            data = reply.readAll()
+
+            original_query = reply.request().url().toString()
+            zoom = int(original_query.split("/")[3])
+            x = int(original_query.split("/")[4])
+            y = int(original_query.split("/")[5][:-4])
+            print(zoom, x, y)
+
+            image = QImage.fromData(data)
+            self.setPixmap(QPixmap.fromImage(image))
+        except Exception as e:
+            print("Response handling error:", e)
+        finally:
+            reply.deleteLater()
